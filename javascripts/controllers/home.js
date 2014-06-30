757cc50c0991fac0b12b50f8c40560c02d5780bf
songawee <dennis@songawee.com>
2014-06-30 06:21:52 -0600

    Update files

--- javascripts/controllers/home.js
+++ javascripts/controllers/home.js
@@ -1,5 +1,5 @@
 angular.module('application').controller('HomePageController', ['$scope', function($scope) {
   $scope.greeting = 'Hola!';
   $scope.data = new Array();
-  $scope.data[0] = [40, 19, 10, 80];
+  $scope.data[0] = [30, 45, 60, 90];
 }]);
